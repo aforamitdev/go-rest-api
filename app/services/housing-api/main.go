e18f2c75f5f8a865e84d727b903a7f9ea4eee5c1
activationeuron <aforamitrai@gmail.com>
2022-10-04 01:52:00 +0530

    added config

--- app/services/housing-api/main.go
+++ app/services/housing-api/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"errors"
+	"expvar"
 	"fmt"
 	"os"
 	"projects/khomeawayserver/foundation/logger"
+	"time"
 
+	_ "expvar"
+
+	"github.com/ardanlabs/conf/v3"
 	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 )
+
+var build = "develop"
 
 func main() {
 
@@ -32,6 +40,56 @@ func run(log *zap.SugaredLogger) error {
 	if _, err := maxprocs.Set(opt); err != nil {
 		log.Errorf("maxprocess: %w", err)
 	}
+
+	// configurations
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+		DB struct {
+			User         string `conf:"default:postgres"`
+			Password     string `conf:"default:postgres,mask"`
+			Host         string `conf:"default:localhost"`
+			Name         string `conf:"default:postgres"`
+			MaxIdleConns int    `conf:"default:0"`
+			MaxOpenConns int    `conf:"default:0"`
+			DisableTLS   bool   `conf:"default:true"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "copyright information here",
+		},
+	}
+
+	const prefix = "HOUSING"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config : %w", err)
+	}
+
+	log.Infow("starting service", "version", build)
+	defer log.Infow("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Infow("startup", "config", out)
+	// new string
+	expvar.NewString("build").Set(build)
+
+	log.Infow("startup", "status", "initializing authentication support")
 
 	return nil
 }
