ed2759d776d7a1ad598880724041f8d07ddc43ba
activationeuron <aforamitrai@gmail.com>
2022-10-04 01:36:56 +0530

    added basic log! and maxprocs

--- app/services/housing-api/main.go
+++ app/services/housing-api/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"projects/khomeawayserver/foundation/logger"
 
+	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 )
 
@@ -17,9 +18,20 @@ func main() {
 		os.Exit(1)
 	}
 	defer log.Sync()
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+	}
 
 }
 
 func run(log *zap.SugaredLogger) error {
 
+	opt := maxprocs.Logger(log.Infof)
+
+	if _, err := maxprocs.Set(opt); err != nil {
+		log.Errorf("maxprocess: %w", err)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module projects/khomeawayserver
 go 1.18
 
 require (
+	github.com/ardanlabs/conf/v3 v3.1.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/automaxprocs v1.5.1 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
 )
