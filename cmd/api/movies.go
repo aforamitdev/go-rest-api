13e665131753f623ad46e7a8819d03fac7bddcd9
aforamitdev <aforamitrai@gmail.com>
2023-09-18 00:59:19 +0530

    initial cconfigutaions, handle errors

---
+++ cmd/api/movies.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"apexapi.aforamitdev.com/internal/data"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{ID: id, CreatedAt: time.Now(), Title: "Casablance", Runtime: 102, Genres: []string{"drama", "romance", "war"}, Version: 1}
+
+	err = app.writeJSON(w, http.StatusOK, envelop{"movie": movie}, nil)
+
+	if err != nil || id < 1 {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
